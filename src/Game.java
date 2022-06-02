ef55d97b252760a511b06b76a201e144fc0f3725
gigi11111 <redede1@gmail.com>
2022-06-02 13:01:28 +0200

    afficher la cible : fait

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -10,15 +14,17 @@ public class Game {
     public static int nbdejoueur=0;
     static JFrame f = new JFrame("carte de jeu");
     protected static int x1;
-    protected static int y1;// xf et yf sont les coordonnées du robot 1 protected static int x1;
+    protected static int y1;// x1 et y1 sont les coordonnées du robot rouge
     protected static int x2;
-    protected static int y2;// xf et yf sont les coordonnées du robot 1 protected static int x1;
+    protected static int y2;// x2 et y2 sont les coordonnées du robot bleu
     protected static int x3;
-    protected static int y3;// xf et yf sont les coordonnées du robot 1 protected static int x1;
+    protected static int y3;// x3 et y3 sont les coordonnées du robot jaune
     protected static int x4;
-    protected static int y4;// xf et yf sont les coordonnées du robot 1
+    protected static int y4;// x4 et y4 sont les coordonnées du robot vert
+    protected static int x,y; //coordonnées de la cible
     public static boolean bool = false;
-    public static void playGame() throws InterruptedException {
+    public static boolean fin = true;
+    public static void playGame() throws InterruptedException, IOException {
         /*nombredejoueur();
         while(nbdejoueur==0){
             Thread.sleep(500);
@@ -26,7 +32,7 @@ public class Game {
         do{
             x1 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
             y1 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
-            if((x1 == 8 || x1==9) && (y1 == 8 || y1==9)){
+            if((x1 == 8 || x1==7) && (y1 == 8 || y1==7)){
                 bool = true;
             }else{
                 bool = false;
@@ -35,33 +41,57 @@ public class Game {
         do{
             x2 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
             y2 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
-            if((x2 == 8 || x2==9) && (y2 == 8 || y2==9)){
+            if((x2 == 8 || x2==7) && (y2 == 8 || y2==7)){
                 bool = true;
             }else{
-                bool = false;
+                if(x2==x1 && y2==y1){
+                    bool = true;
+                }else {
+                    bool = false;
+                }
             }
         }while (bool);
         do{
             x3 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
             y3 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
-            if((x3 == 8 || x3==9) && (y3 == 8 || y3==9)){
+            if((x3 == 8 || x3==7) && (y3 == 8 || y3==7)){
                 bool = true;
-            }else{
-                bool = false;
+            }else {
+                if (x3 == x1 && y3 == y1) {
+                    bool = true;
+                } else if (x3 == x2 && y3 == y2) {
+                    bool = true;
+                } else {
+                    bool = false;
+                }
             }
         }while (bool);
         do{
             x4 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
             y4 = ThreadLocalRandom.current().nextInt(0, 15 + 1);
-            if((x4 == 8 || x4==9) && (y4 == 8 || y4==9)){
+            if((x4 == 7 || x4==8) && (y4 == 7 || y4==8)){
                 bool = true;
-            }else{
-                bool = false;
+            }else {
+                if (x4 == x1 && y4 == y1) {
+                    bool = true;
+                } else if (x4 == x2 && y4 == y2) {
+                    bool = true;
+                }else if (x4 == x3 && y4 == y3) {
+                bool = true;
+            } else {
+                    bool = false;
+                }
             }
         }while (bool);
-        System.out.println(x1+" "+y1);
-        System.out.println(x2+" "+y2);
-        affichejeu();
+        System.out.println(" "+ x1 +" "+ y1 +" "+ x2 +" "+ y2 + " "+ x3 + " "+y3 +" "+ x4 + " "+y4);
+        init(true);
+        while(fin) {
+            init(false);
+            affichejeu();
+            couleur();
+            Thread.sleep(30000);
+        }
+        Main.fin();//Victoire
     }
     public static void nombredejoueur(){
         JFrame frame = new JFrame("frame");
@@ -113,6 +143,159 @@ public class Game {
         f.setVisible(true);
         f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void init(boolean bool){
+       if(bool){
+           P.x11 = x1;
+           P.y11 = y1;
+           P.x22 = x2;
+           P.y22 = y2;
+           P.x33 = x3;
+           P.y33 = y3;
+           P.x44 = x4;
+           P.y44 = y4;
+       }else{
+           x1 = P.x11;
+           y1 = P.y11;
+           x2 = P.x22;
+           y2 = P.y22;
+           x3 = P.x33;
+           y3 = P.y33;
+           x4 = P.x44;
+           y4 = P.y44;
+       }
+    }
+    public static void couleur() throws IOException {
+        int k = ThreadLocalRandom.current().nextInt(1, 15 + 1);
+        Frame jfond = new Frame("cible à viser");
+        jfond.setSize(150,150);
+        JPanel panel = new JPanel();
+        panel.setBounds(800, 200, 200, 200);
+        BufferedImage img;
+        System.out.println(k);
+        switch (k){
+            case 1:
+                x = 1;
+                y = 5;
+                img = ImageIO.read(new File("src\\cible15.jpg"));
+                JLabel pic = new JLabel(new ImageIcon(img));
+                panel.add(pic);
+                jfond.add(panel);
+                break;
+            case 2 :
+                x = 4;
+                y = 7;
+                img = ImageIO.read(new File("src\\cible47.jpg"));
+                JLabel pic1 = new JLabel(new ImageIcon(img));
+                panel.add(pic1);
+                jfond.add(panel);
+                break;
+            case 3 :
+                x = 5;
+                y = 2;
+                img = ImageIO.read(new File("src\\cible52.jpg"));
+                JLabel pic2 = new JLabel(new ImageIcon(img));
+                panel.add(pic2);
+                jfond.add(panel);
+                break;
+            case 4 :
+                x = 9;
+                y = 1;
+                img = ImageIO.read(new File("src\\cible91.jpg"));
+                JLabel pic3 = new JLabel(new ImageIcon(img));
+                panel.add(pic3);
+                jfond.add(panel);
+                break;
+            case 5:
+                x = 9;
+                y = 4;
+                img = ImageIO.read(new File("src\\cible94.jpg"));
+                JLabel pic5 = new JLabel(new ImageIcon(img));
+                panel.add(pic5);
+                jfond.add(panel);
+                break;
+            case 6 :
+                x = 10;
+                y = 6;
+                img = ImageIO.read(new File("src\\cible106.jpg"));
+                JLabel pic6 = new JLabel(new ImageIcon(img));
+                panel.add(pic6);
+                jfond.add(panel);
+                break;
+            case 7 :
+                x = 10;
+                y = 9;
+                img = ImageIO.read(new File("src\\cible109.jpg"));
+                JLabel pic7 = new JLabel(new ImageIcon(img));
+                panel.add(pic7);
+                jfond.add(panel);
+                break;
+            case 8:
+                x = 11;
+                y = 3;
+                img = ImageIO.read(new File("src\\cible113.jpg"));
+                JLabel pic8 = new JLabel(new ImageIcon(img));
+                panel.add(pic8);
+                jfond.add(panel);
+                break;
+            case 9:
+                x = 14;
+                y = 5;
+                img = ImageIO.read(new File("src\\cible145.jpg"));
+                JLabel pic9 = new JLabel(new ImageIcon(img));
+                panel.add(pic9);
+                jfond.add(panel);
+                break;
+            case 10 :
+                x = 3;
+                y = 14;
+                img = ImageIO.read(new File("src\\cible314.jpg"));
+                JLabel pic10 = new JLabel(new ImageIcon(img));
+                panel.add(pic10);
+                jfond.add(panel);
+                break;
+            case 11 :
+                x = 6;
+                y = 12;
+                img = ImageIO.read(new File("src\\cible612.jpg"));
+                JLabel pic11 = new JLabel(new ImageIcon(img));
+                panel.add(pic11);
+                jfond.add(panel);
+                break;
+            case 12:
+                x = 6;
+                y = 13;
+                img = ImageIO.read(new File("src\\cible613.jpg"));
+                JLabel pic12 = new JLabel(new ImageIcon(img));
+                panel.add(pic12);
+                jfond.add(panel);
+                break;
+            case 13:
+                x = 9;
+                y = 13;
+                img = ImageIO.read(new File("src\\cible913.jpg"));
+                JLabel pic13 = new JLabel(new ImageIcon(img));
+                panel.add(pic13);
+                jfond.add(panel);
+                break;
+            case 14 :
+                x = 12;
+                y = 11;
+                img = ImageIO.read(new File("src\\cible1211.jpg"));
+                JLabel pic14 = new JLabel(new ImageIcon(img));
+                panel.add(pic14);
+                jfond.add(panel);
+                break;
+            case 15:
+                x = 13;
+                y = 11;
+                img = ImageIO.read(new File("src\\cible15.jpg"));
+                JLabel pic15 = new JLabel(new ImageIcon(img));
+                panel.add(pic15);
+                jfond.add(panel);
+                break;
+        }
+        jfond.setVisible(true);
     }
 
 }
---
+++ src/P.java
@@ -0,0 +1,10 @@
+public class P {
+    protected static int x11;
+    protected static int y11;// xf et yf sont les coordonnées du robot 1 protected static int x1;
+    protected static int x22;
+    protected static int y22;// xf et yf sont les coordonnées du robot 1 protected static int x1;
+    protected static int x33;
+    protected static int y33;// xf et yf sont les coordonnées du robot 1 protected static int x1;
+    protected static int x44;
+    protected static int y44;// xf et yf sont les coordonnées du robot 1
+}
